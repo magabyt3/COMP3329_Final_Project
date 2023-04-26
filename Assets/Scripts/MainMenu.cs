48d8da6c6895e6447dbb5682604c4f7951280629
magabyt3 <maggieshao27@gmail.com>
2023-04-26 16:24:02 +0800

    Add files until Level 4, Main Menu and Levels Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void LevelSelector()
+    {
+        SceneManager.LoadScene("Levels Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
